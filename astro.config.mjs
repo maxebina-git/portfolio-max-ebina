26733754cbdd4eb38326986b4816e8a8547fdd93
maxebina <max@maxebina.com.br>
2026-05-19 11:40:19 -0300

    fix: base path inteligente para localhost e producao

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react'; // 👈 Reimportando o plugin do React
+import react from '@astrojs/react';
 
-// https://astro.build/config
+// O Astro injeta automaticamente "production" quando o robô do GitHub roda o build
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  integrations: [react()], // 👈 Ativando o motor do React no Astro!
+  site: 'https://www.maxebina.com.br',
+  // 👇 Se for produção, usa a subpasta. Se for localhost, roda na raiz normalmente!
+  base: isProd ? '/portfolio-teste' : '/', 
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
   }
