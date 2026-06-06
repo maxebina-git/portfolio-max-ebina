961860709efde48bdbfb67dede7087fe5acf5edb
Max Ebina <maxebina@gmail.com>
2026-06-06 14:37:10 -0300

    teste email php

--- public/send-email.php
+++ public/send-email.php
@@ -1,7 +1,5 @@
 <?php
 
-echo "PHP OK<br>";
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -9,12 +7,46 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-echo "PHPMailer OK<br>";
+echo "PHP OK<br>";
 
 $mail = new PHPMailer(true);
 
 $mail->isSMTP();
 
-echo "SMTP OK<br>";
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'maxebina@gmail.com';
+$mail->Password = 'SUA_SENHA_APP';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
 
-exit;
+echo "CONFIG SMTP OK<br>";
+
+$mail->setFrom(
+    'maxebina@gmail.com',
+    'Portfolio Max'
+);
+
+$mail->addAddress(
+    'maxebina@gmail.com'
+);
+
+$mail->isHTML(true);
+
+$mail->Subject = 'Teste';
+
+$mail->Body = 'Teste';
+
+echo "ANTES SEND<br>";
+
+try {
+
+    $mail->send();
+
+    echo "DEPOIS SEND<br>";
+
+} catch (Exception $e) {
+
+    echo "ERRO:<br>";
+    echo $mail->ErrorInfo;
+}
