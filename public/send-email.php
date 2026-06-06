d5db9d7f395467127bad6102e7dbfb265a600a76
Max Ebina <maxebina@gmail.com>
2026-06-06 14:00:33 -0300

    teste email php

--- public/send-email.php
+++ public/send-email.php
@@ -1,18 +1,43 @@
 <?php
 
-echo "PASSOU 1<br>";
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
-echo "PASSOU 2<br>";
 
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-echo "PASSOU 3<br>";
-
 $mail = new PHPMailer(true);
 
+echo "PASSOU 1<br>";
+
+$mail->isSMTP();
+
+echo "PASSOU 2<br>";
+
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+
+echo "PASSOU 3<br>";
+
+$mail->Username = 'maxebina@gmail.com';
+$mail->Password = 'SUA_SENHA';
+
 echo "PASSOU 4<br>";
+
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+echo "PASSOU 5<br>";
+
+$mail->CharSet = 'UTF-8';
+$mail->Encoding = 'base64';
+
+echo "PASSOU 6<br>";
+
+$mail->Subject = mb_encode_mimeheader(
+    'Novo contato do portfólio',
+    'UTF-8'
+);
+
+echo "PASSOU 7<br>";
