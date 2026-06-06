abf0c8027f3a772f9bb309a5867136f4ac6eed8f
Max Ebina <maxebina@gmail.com>
2026-06-06 14:38:46 -0300

    teste email php

--- public/send-email.php
+++ public/send-email.php
@@ -16,7 +16,7 @@ $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->Username = 'maxebina@gmail.com';
-$mail->Password = 'SUA_SENHA_APP';
+$mail->Password = 'vwtv peed tjro ibsg';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 
