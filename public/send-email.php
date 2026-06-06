4cf0e1c48c3cef70cd0c59c806dbcb61ad16b3d9
Max Ebina <maxebina@gmail.com>
2026-06-06 18:03:38 -0300

    adiciona feedback visual de sucesso no envio

--- public/send-email.php
+++ public/send-email.php
@@ -125,7 +125,7 @@ try {
 
     // 🚀 REDIRECT APÓS SUCESSO
     header(
-    "Location: https://www.maxebina.com.br/conectar"
+    "Location: https://www.maxebina.com.br/#conectar"
     );
     exit;
 
